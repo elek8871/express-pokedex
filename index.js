ad1e913dcd5e7e3377d9a5bf71df9e73415873b4
elek8871 <elekmichelle@gmail.com>
2022-09-08 15:52:53 -0600

    lab

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const axios = require('axios'); 
 const ejsLayouts = require('express-ejs-layouts');
+const db = require('./models')
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -18,6 +19,50 @@ app.get('/', (req, res) => {
     res.render('index', { pokemon: pokemon.slice(0, 151) });
   })
 });
+// GET /pokemon retrieve all favs
+app.get ('/pokemon', async (req,res)=>{
+  try{
+    const allFaves= await db.pokemon.findAll()
+    res.render ('faves.ejs', {
+      allFaves
+    })  
+  } catch (err){
+    console.log(err)
+    res.send('server error')
+  }
+
+})
+
+// POST /pokemon
+app.post('/pokemon', async (req, res)=>{
+  // res.send ('create a new fav')
+  try{
+    await db.pokemon.create(req.body)
+    res.redirect('/pokemon')
+  } catch(err){
+   console.log(err)
+   res.send ('pokemon error')
+  }
+})
+
+// GET /pokemon/:name -> renders a show page w poke info
+
+app.get('/pokemon/:name', async (req,res) => {
+  try {
+    // console.log(req.params.name)
+    const url = `http://pokeapi.co/api/v2/pokemon/${req.params.name}/`
+    axios.get(url)
+      .then(response => {
+      let pokemon = response.data.species;
+      res.render('show.ejs', { pokemon })
+      // console.log(response.data.species)
+    })
+  }catch(err){
+    console.log(err)
+    res.send('pokemon error')
+  }
+  // res.send('details about certain pokemon')
+})
 
 // Imports all routes from the pokemon routes file
 app.use('/pokemon', require('./routes/pokemon'));
